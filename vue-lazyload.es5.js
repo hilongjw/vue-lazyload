b6a3c6b965ea2d498434341dc60fa90336123574
Awe <hilongjw@gmail.com>
2016-09-07 22:17:37 +0800

    fix: issue#35

--- vue-lazyload.es5.js
+++ vue-lazyload.es5.js
@@ -2,6 +2,31 @@
 
 var Promise = require('es6-promise').Promise;
 
+if (!Array.prototype.find) {
+    Array.prototype.find = function (predicate) {
+        'use strict';
+
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
 exports.install = function (Vue, Options) {
     var isVueNext = Vue.version.split('.')[0] === '2';
     var DEFAULT_PRE = 1.3;
