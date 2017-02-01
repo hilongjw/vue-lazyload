e5de921482fbfb9ca3b9cf94b8e6ea9b3f96eb78
maggiehe <mechi.he@gmail.com>
2017-02-01 10:06:05 +0800

    Revert "config options dynamically"
    This reverts commit 93fecb6c466428d83bcd9299092d4c71957c69df.
    
    revert config options dynamically

--- src/index.js
+++ src/index.js
@@ -2,51 +2,37 @@ import Lazy from './lazy'
 import LazyComponent from './lazy-component'
 import { assign } from './util'
 
-let lazy = null
-let getLazyInstance = () => {
-    if (!!lazy) {
-        return lazy
-    }
-    lazy = new Lazy({})
-    return lazy
-}
+export default (Vue, options = {}) => {
+    const lazy = new Lazy(options)
+    const isVueNext = Vue.version.split('.')[0] === '2'
 
-export default {
-    install(Vue, options = {}) {
-        Object.assign(getLazyInstance().options, options)
-        const isVueNext = Vue.version.split('.')[0] === '2'
+    Vue.prototype.$Lazyload = lazy
+    Vue.component('lazy-component', LazyComponent(lazy))
 
-        Vue.prototype.$Lazyload = lazy
-        Vue.component('lazy-component', LazyComponent(lazy))
-
-        if (isVueNext) {
-            Vue.directive('lazy', {
-                bind: lazy.add.bind(lazy),
-                update: lazy.update.bind(lazy),
-                componentUpdated: lazy.lazyLoadHandler.bind(lazy),
-                unbind : lazy.remove.bind(lazy)
-            })
-        } else {
-            Vue.directive('lazy', {
-                bind: lazy.lazyLoadHandler.bind(lazy),
-                update (newValue, oldValue) {
-                    assign(this.vm.$refs, this.vm.$els)
-                    lazy.add(this.el, {
-                        modifiers: this.modifiers || {},
-                        arg: this.arg,
-                        value: newValue,
-                        oldValue: oldValue
-                    }, {
-                        context: this.vm
-                    })
-                },
-                unbind () {
-                    lazy.remove(this.el)
-                }
-            })
-        }
-    },
-    config(options = {}) {
-        Object.assign(getLazyInstance().options, options)
+    if (isVueNext) {
+        Vue.directive('lazy', {
+            bind: lazy.add.bind(lazy),
+            update: lazy.update.bind(lazy),
+            componentUpdated: lazy.lazyLoadHandler.bind(lazy),
+            unbind : lazy.remove.bind(lazy)
+        })
+    } else {
+        Vue.directive('lazy', {
+            bind: lazy.lazyLoadHandler.bind(lazy),
+            update (newValue, oldValue) {
+                assign(this.vm.$refs, this.vm.$els)
+                lazy.add(this.el, {
+                    modifiers: this.modifiers || {},
+                    arg: this.arg,
+                    value: newValue,
+                    oldValue: oldValue
+                }, {
+                    context: this.vm
+                })
+            },
+            unbind () {
+                lazy.remove(this.el)
+            }
+        })
     }
 }
