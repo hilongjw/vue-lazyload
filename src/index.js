d897b713fcea1c5588aac649d074533c6ab823c1
dajiza <chensa222@gmail.com>
2021-10-09 19:01:55 +0800

    test-pr

--- src/index.js
+++ src/index.js
@@ -1,85 +1,88 @@
-import Lazy from './lazy'
-import LazyComponent from './lazy-component'
-import LazyContainer from './lazy-container'
-import LazyImage from './lazy-image'
-import { assign } from './util'
-
+import Lazy from "./lazy";
+import LazyComponent from "./lazy-component";
+import LazyContainer from "./lazy-container";
+import LazyImage from "./lazy-image";
+import { assign } from "./util";
+// test
 export default {
-  /*
-  * install function
-  * @param  {Vue} Vue
-  * @param  {object} options  lazyload options
-  */
-  install (Vue, options = {}) {
-    const LazyClass = Lazy(Vue)
-    const lazy = new LazyClass(options)
-    const lazyContainer = new LazyContainer({ lazy })
+    /*
+     * install function
+     * @param  {Vue} Vue
+     * @param  {object} options  lazyload options
+     */
+    install(Vue, options = {}) {
+        const LazyClass = Lazy(Vue);
+        const lazy = new LazyClass(options);
+        const lazyContainer = new LazyContainer({ lazy });
 
-    const isVue2 = Vue.version.split('.')[0] === '2'
+        const isVue2 = Vue.version.split(".")[0] === "2";
 
-    Vue.prototype.$Lazyload = lazy
+        Vue.prototype.$Lazyload = lazy;
 
-    if (options.lazyComponent) {
-      Vue.component('lazy-component', LazyComponent(lazy))
-    }
-
-    if (options.lazyImage) {
-      Vue.component('lazy-image', LazyImage(lazy))
-    }
-
-    if (isVue2) {
-      Vue.directive('lazy', {
-        bind: lazy.add.bind(lazy),
-        update: lazy.update.bind(lazy),
-        componentUpdated: lazy.lazyLoadHandler.bind(lazy),
-        unbind: lazy.remove.bind(lazy)
-      })
-      Vue.directive('lazy-container', {
-        bind: lazyContainer.bind.bind(lazyContainer),
-        componentUpdated: lazyContainer.update.bind(lazyContainer),
-        unbind: lazyContainer.unbind.bind(lazyContainer)
-      })
-    } else {
-      Vue.directive('lazy', {
-        bind: lazy.lazyLoadHandler.bind(lazy),
-        update (newValue, oldValue) {
-          assign(this.vm.$refs, this.vm.$els)
-          lazy.add(this.el, {
-            modifiers: this.modifiers || {},
-            arg: this.arg,
-            value: newValue,
-            oldValue: oldValue
-          }, {
-            context: this.vm
-          })
-        },
-        unbind () {
-          lazy.remove(this.el)
+        if (options.lazyComponent) {
+            Vue.component("lazy-component", LazyComponent(lazy));
         }
-      })
 
-      Vue.directive('lazy-container', {
-        update (newValue, oldValue) {
-          lazyContainer.update(this.el, {
-            modifiers: this.modifiers || {},
-            arg: this.arg,
-            value: newValue,
-            oldValue: oldValue
-          }, {
-            context: this.vm
-          })
-        },
-        unbind () {
-          lazyContainer.unbind(this.el)
+        if (options.lazyImage) {
+            Vue.component("lazy-image", LazyImage(lazy));
         }
-      })
-    }
-  }
-}
 
-export {
-  Lazy,
-  LazyComponent,
-  LazyImage,
-  LazyContainer
-}
+        if (isVue2) {
+            Vue.directive("lazy", {
+                bind: lazy.add.bind(lazy),
+                update: lazy.update.bind(lazy),
+                componentUpdated: lazy.lazyLoadHandler.bind(lazy),
+                unbind: lazy.remove.bind(lazy),
+            });
+            Vue.directive("lazy-container", {
+                bind: lazyContainer.bind.bind(lazyContainer),
+                componentUpdated: lazyContainer.update.bind(lazyContainer),
+                unbind: lazyContainer.unbind.bind(lazyContainer),
+            });
+        } else {
+            Vue.directive("lazy", {
+                bind: lazy.lazyLoadHandler.bind(lazy),
+                update(newValue, oldValue) {
+                    assign(this.vm.$refs, this.vm.$els);
+                    lazy.add(
+                        this.el,
+                        {
+                            modifiers: this.modifiers || {},
+                            arg: this.arg,
+                            value: newValue,
+                            oldValue: oldValue,
+                        },
+                        {
+                            context: this.vm,
+                        }
+                    );
+                },
+                unbind() {
+                    lazy.remove(this.el);
+                },
+            });
+
+            Vue.directive("lazy-container", {
+                update(newValue, oldValue) {
+                    lazyContainer.update(
+                        this.el,
+                        {
+                            modifiers: this.modifiers || {},
+                            arg: this.arg,
+                            value: newValue,
+                            oldValue: oldValue,
+                        },
+                        {
+                            context: this.vm,
+                        }
+                    );
+                },
+                unbind() {
+                    lazyContainer.unbind(this.el);
+                },
+            });
+        }
+    },
+};
+
+export { Lazy, LazyComponent, LazyImage, LazyContainer };
